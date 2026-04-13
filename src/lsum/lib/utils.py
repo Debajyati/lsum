ba3a9fc169f26f460b498da24dbda66930139a2a
Debajyati Dey <ddebajyati@gmail.com>
2026-04-14 01:06:02 +0530

    feat: other utility functions

---
+++ src/lsum/lib/utils.py
@@ -0,0 +1,20 @@
+from typing import Any, Tuple
+
+def assoclist(array1:list[Any], array2:list[Any]):
+    result:list[Tuple[Any,Any]] = []
+
+    length = min(len(array1),len(array2))
+    for i in range(length):
+        result.append((array1[i],array2[i]))
+    return result
+
+colormap = {
+    "red",
+    "green",
+    "blue",
+    "yellow",
+    "cyan",
+    "magenta",
+    "black",
+    "white"
+}
